e1d5d68b6cb77ef0c15f0d162ed166878ff486de
Yug063 <tyagiyug2018@gmail.com>
2024-06-07 18:44:26 +0530

    second commit

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Box } from "@mui/material";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <img
+        src="/404.png" // Ensure this path is correct and the image is in the public folder
+        alt="404 Not Found"
+        style={{ width: "600px", display: "block", margin: "auto" }}
+      />
+    </Box>
+  );
 };
 
 export default NotFound;
